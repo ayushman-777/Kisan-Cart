0975cdba43ad96d6688aba8ca73a9830ee7b4085
Hunter-777 <59577881+Hunter-777@users.noreply.github.com>
2020-11-30 14:17:51 +0530

    initial commits 2.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {SellerFormComponent} from './seller-form/seller-form.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {LoginComponent} from './login/login.component';
+import {ProductsComponent} from './products/products.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'seller-form', component: SellerFormComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'buy', component: ProductsComponent},
+  {path: 'sign-up', component: SignUpComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, SellerFormComponent, AboutUsComponent, LoginComponent, ProductsComponent, SignUpComponent];
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC9maJQU88i688Sm6_tj9cigdgBZKIZStk',
+    authDomain: 'kisancart-945f6.firebaseapp.com',
+    databaseURL: 'https://kisancart-945f6.firebaseio.com',
+    projectId: 'kisancart-945f6',
+    storageBucket: 'kisancart-945f6.appspot.com',
+    messagingSenderId: '213794672792',
+    appId: '1:213794672792:web:a61dbcf4920c689c8980ed'
+  }
 };
