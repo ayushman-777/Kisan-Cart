fa0a1161abaf88d7ac650c4f263b8864ec977ef0
varad <varadkhokle10@gmail.com>
2020-12-15 22:36:21 +0530

    changed aboutus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import {AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CropDetailsComponent } from './crop-details/crop-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    CropDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/crop-details/crop-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crop-details',
+  templateUrl: './crop-details.component.html',
+  styleUrls: ['./crop-details.component.css']
+})
+export class CropDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
